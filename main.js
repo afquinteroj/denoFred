46e4ac998dfff0c51c709ac6a20e2e404aefad67
Alfie <afquinteroj@pm.me>
2022-05-30 14:27:16 -0700

    category setup and first method

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //import FRED_API from './lib/Fred.js'
-/*import CategoryFred from './lib/categoryFred.js'
-import ReleasesFred from './lib/releasesFred.js'*/
+import CategoryFred from './lib/categoryFred.js'
+/*import ReleasesFred from './lib/releasesFred.js'*/
 import SeriesFred from './lib/seriesFred.js'
 import SourcesFred from './lib/sourcesFred.js'
 import TagsFred from './lib/tagsFred.js'
 
-
+export {CategoryFred};
 export {SeriesFred};
 export {TagsFred};
 export {SourcesFred};
